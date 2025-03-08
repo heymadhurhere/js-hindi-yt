9ee2672cc7dd781baa719ef9dedccef78035535b
heymadhurhere <162946777+heymadhurhere@users.noreply.github.com>
2025-03-08 15:29:03 +0000

    converison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,3 +17,31 @@ console.log(typeof (newNumber))
 let value = "";
 let boolvalue = Boolean(value);
 console.log(boolvalue);
+
+// *********************** Operations ***********************
+let num = 3;
+let negValue = -num;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2); // power
+// console.log(2%2); // remainder
+
+// let str1 = "Madhur"
+// let str2 = "Anand"
+// let str3 = str1 + " " + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+
+let game = 100;
+++game;
+console.log(game);
