7de3dd460340d1592f03055d6068465e0e44f252
heymadhurhere <162946777+heymadhurhere@users.noreply.github.com>
2025-03-11 06:07:42 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Mar"
+
+switch (month) {
+    case "Jan":
+        console.log("January");  
+        break;
+    case "Feb":
+        console.log("February");  
+        break; // if break not implemented, all code after break will be executed
+    case "Mar":
+        console.log("March");  
+        break;
+
+    default:
+        break;
+}
